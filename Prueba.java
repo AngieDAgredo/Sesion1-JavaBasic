eefac1b945b6aa47a5a3dcffefb860e35e1e27da
Angie Daniela Agredo Gómez <adaniela2204@gmail.com>
2024-03-19 16:26:27 -0500

    inicial

---
+++ Prueba.java
@@ -0,0 +1,38 @@
+package Task1;
+
+import javax.swing.*;
+
+public class Prueba {
+
+    public static void main(String[] args) {
+
+        byte var1 = 4;
+        System.out.println(var1);
+        short var2 = 12;
+        System.out.println(var2);
+        int var3 = 22;
+        System.out.println(var3);
+        long var4 = 1997;
+        System.out.println(var4);
+
+        float var5 = 1.64f;
+        System.out.println(var5);
+        double var6 = 199.7d;
+        System.out.println(var6);
+        boolean var7 = false ;
+        System.out.println(var7);
+        boolean var8 = true;
+        System.out.println(var8);
+
+        char var9 = 'a';
+        System.out.println(var9);
+        String var10 = "Angie Daniela Agredo";
+        System.out.println(var10);
+
+
+
+
+    }
+}
+
+
